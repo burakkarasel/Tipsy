46bbf95820736978aca7b4d22a5633f3265008c6
Burak Can Karasel <99825584+burakkarasel@users.noreply.github.com>
2023-08-13 22:38:52 +0300

    Added on click backgroundColor and tintColor changes to tip percent choice

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -21,6 +21,8 @@ class CalculatorViewController: UIViewController {
     }
 
     @IBAction func tipButtonPressed(_ sender: UIButton) {
+        guard let title = sender.titleLabel?.text! else {return}
+        toggleStyling(title: title)
     }
     
     @IBAction func stepperPressed(_ sender: UIStepper) {
@@ -28,6 +30,37 @@ class CalculatorViewController: UIViewController {
     
     @IBAction func calculateButtonPressed(_ sender: UIButton){
         
+    }
+    
+    func toggleStyling(title : String) {
+        
+        let darkPurple = UIColor(red: CGFloat(0.2901960784313726), green: CGFloat(0.3333333333333333), blue: CGFloat(0.6352941176470588), alpha: CGFloat(1))
+        let white = UIColor(red: CGFloat(1), green: CGFloat(1), blue: CGFloat(1), alpha: CGFloat(1))
+        let transparent = UIColor.white.withAlphaComponent(CGFloat(0))
+        
+        switch title {
+        case "%0":
+            zeroPercentTipButton.tintColor = white
+            zeroPercentTipButton.backgroundColor = darkPurple
+            tenPercentTipButton.backgroundColor = transparent
+            tenPercentTipButton.tintColor = darkPurple
+            twentyPercentTipButton.backgroundColor = transparent
+            twentyPercentTipButton.tintColor = darkPurple
+        case "%10":
+            tenPercentTipButton.tintColor = white
+            tenPercentTipButton.backgroundColor = darkPurple
+            zeroPercentTipButton.backgroundColor = transparent
+            zeroPercentTipButton.tintColor = darkPurple
+            twentyPercentTipButton.backgroundColor = transparent
+            twentyPercentTipButton.tintColor = darkPurple
+        default:
+            twentyPercentTipButton.tintColor = white
+            twentyPercentTipButton.backgroundColor = darkPurple
+            zeroPercentTipButton.backgroundColor = transparent
+            zeroPercentTipButton.tintColor = darkPurple
+            tenPercentTipButton.backgroundColor = transparent
+            tenPercentTipButton.tintColor = darkPurple
+        }
     }
 }
 
